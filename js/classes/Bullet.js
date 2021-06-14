8e208a7d67b33ec38d1bd084ab302576b0ba56ab
Thomas Hockaday <thomashockaday@intelligencefusion.co.uk>
2021-06-14 12:43:38 +0100

    Move Bullet class into its own file

---
+++ js/classes/Bullet.js
@@ -0,0 +1,44 @@
+import Stage from './Stage.js';
+
+class Bullet {
+  radius = 8;
+
+  speed = 10;
+
+  finished = false;
+
+  constructor(x, y, velocity) {
+    this.x = x + velocity.x * 60;
+    this.y = y + velocity.y * 60;
+    this.velocity = velocity;
+  }
+
+  draw(ctx) {
+    ctx.fillStyle = 'red';
+    ctx.beginPath();
+    ctx.arc(
+      this.x,
+      this.y,
+      this.radius,
+      0,
+      Math.PI * 2,
+    );
+    ctx.fill();
+  }
+
+  update() {
+    this.x += this.velocity.x * this.speed;
+    this.y += this.velocity.y * this.speed;
+
+    if (
+      this.x > Stage.width
+      || this.x + this.radius < 0
+      || this.y < 0
+      || this.y + this.radius > Stage.height
+    ) {
+      this.finished = true;
+    }
+  }
+}
+
+export default Bullet;
--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 import circleCollision from './utils.js';
 
+import Bullet from './classes/Bullet.js';
 import Camera from './classes/Camera.js';
 import Enemy from './classes/Enemy.js';
 import Stage from './classes/Stage.js';
@@ -103,47 +104,6 @@ class Player {
 
   rotate(event) {
     this.angle = Math.atan2(event.clientY - canvas.height / 2, event.clientX - canvas.width / 2);
-  }
-}
-
-class Bullet {
-  radius = 8;
-
-  speed = 10;
-
-  finished = false;
-
-  constructor(x, y, velocity) {
-    this.x = x + velocity.x * 60;
-    this.y = y + velocity.y * 60;
-    this.velocity = velocity;
-  }
-
-  draw(ctx) {
-    ctx.fillStyle = 'red';
-    ctx.beginPath();
-    ctx.arc(
-      this.x,
-      this.y,
-      this.radius,
-      0,
-      Math.PI * 2,
-    );
-    ctx.fill();
-  }
-
-  update() {
-    this.x += this.velocity.x * this.speed;
-    this.y += this.velocity.y * this.speed;
-
-    if (
-      this.x > Stage.width
-      || this.x + this.radius < 0
-      || this.y < 0
-      || this.y + this.radius > Stage.height
-    ) {
-      this.finished = true;
-    }
   }
 }
 
