fc8b176f10bfad5a4ea681df224c2a1cd34db650
Thomas Hockaday <thomashockaday@intelligencefusion.co.uk>
2021-06-14 12:59:24 +0100

    Eslint: Reorder functions to be called after they have been declared

--- js/index.js
+++ js/index.js
@@ -9,12 +9,6 @@ const canvas = document.createElement('canvas');
 canvas.width = 400;
 canvas.height = 400;
 
-function init() {
-  document.body.appendChild(canvas);
-
-  game();
-}
-
 const ctx = canvas.getContext('2d');
 
 const interval = 1000 / 60;
@@ -23,20 +17,11 @@ let then = Date.now();
 const player = new Player();
 const enemies = [];
 
-const game = () => {
-  const now = Date.now();
-  const elapsed = now - then;
+for (let i = 0; i < 7; i += 1) {
+  enemies.push(new Enemy(Math.random() * Stage.width, Math.random() * Stage.height));
+}
 
-  if (elapsed > interval) {
-    then = now - (elapsed % interval);
-    update();
-    draw();
-  }
-
-  requestAnimationFrame(game);
-};
-
-function update() {
+const update = () => {
   player.update();
 
   enemies.forEach((enemy) => {
@@ -53,7 +38,7 @@ function update() {
       enemies.splice(enemies.indexOf(enemy), 1);
     }
   });
-}
+};
 
 const draw = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -73,8 +58,23 @@ const draw = () => {
   ctx.restore();
 };
 
-for (let i = 0; i < 7; i += 1) {
-  enemies.push(new Enemy(Math.random() * Stage.width, Math.random() * Stage.height));
+const game = () => {
+  const now = Date.now();
+  const elapsed = now - then;
+
+  if (elapsed > interval) {
+    then = now - (elapsed % interval);
+    update();
+    draw();
+  }
+
+  requestAnimationFrame(game);
+};
+
+function init() {
+  document.body.appendChild(canvas);
+
+  game();
 }
 
 window.onload = init;
